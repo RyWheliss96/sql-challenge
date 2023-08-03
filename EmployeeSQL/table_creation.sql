6cb12bd968c1e46769d0ba3011b2c2f98a95fd60
RyWheliss96 <rywheliss@gmail.com>
2023-08-03 17:26:49 -0500

    began on creation of Tables and created diagram

---
+++ EmployeeSQL/table_creation.sql
@@ -0,0 +1,62 @@
+-- Drop Tables
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+
+-- Create departments table
+CREATE TABLE departments (
+    dept_no VARCHAR NOT NULL,
+    dept_name VARCHAR NOT NULL,
+    PRIMARY KEY (dept_no)
+);
+
+-- Create titles table
+CREATE TABLE titles (
+    title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+--Create employees table
+CREATE TABLE employees (
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date VARCHAR NOT NULL,
+    PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Create dept_emp table
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Create dept_manager table
+CREATE TABLE dept_manager (
+    dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Create salaries table
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+	salary INT,
+	PRIMARY KEY (emp_no, salary),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+
