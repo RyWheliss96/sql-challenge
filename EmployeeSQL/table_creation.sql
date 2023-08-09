fff242fc3f1edb0f3dd6892bd07479d361d5256d
RyWheliss96 <rywheliss@gmail.com>
2023-08-09 15:29:41 -0500

    Final Changes pt.2

--- EmployeeSQL/table_creation.sql
+++ EmployeeSQL/table_creation.sql
@@ -8,14 +8,14 @@ DROP TABLE IF EXISTS salaries;
 
 -- Create departments table
 CREATE TABLE departments (
-    dept_no VARCHAR NOT NULL,
-    dept_name VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (dept_no)
 );
 
 -- Create titles table
 CREATE TABLE titles (
-    title_id VARCHAR NOT NULL,
+    title_id VARCHAR(5) NOT NULL,
 	title VARCHAR NOT NULL,
 	PRIMARY KEY (title_id)
 );
@@ -23,12 +23,12 @@ CREATE TABLE titles (
 --Create employees table
 CREATE TABLE employees (
     emp_no INT NOT NULL,
-    emp_title_id VARCHAR NOT NULL,
-    birth_date VARCHAR NOT NULL,
+    emp_title_id VARCHAR(5) NOT NULL,
+    birth_date VARCHAR(11) NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
-	hire_date VARCHAR NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date VARCHAR(11) NOT NULL,
     PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
@@ -36,7 +36,7 @@ CREATE TABLE employees (
 -- Create dept_emp table
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
@@ -44,7 +44,7 @@ CREATE TABLE dept_emp (
 
 -- Create dept_manager table
 CREATE TABLE dept_manager (
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
 	PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
